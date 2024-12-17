206a6ae3367011f30a53a37d0031846317c88b00
Muhammad Syafrudin <2568729+justudin@users.noreply.github.com>
2024-12-17 16:07:05 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <Layout
       title={`Applied INTelligence Lab (AINTLab) - Research together with Muhammad Syafrudin - AINTLab.com`}
-      description="Applied INTelligence Lab (AINTLab) - Research together with Muhammad Syafrudin - AINTLab.com - "AINTLab" is not only a laboratory but also a playground to learn and explore things related to applied intelligence and the internet of things. - Applied INTelligence Lab (AINTLab), Department of Artificial Intelligence and Data Science, Sejong University, Seoul 05006, Republic of Korea; Lead by Muhammad Syafrudin.">
+      description="Applied INTelligence Lab (AINTLab) - AINTLab.com is not only a laboratory but also a playground to learn and explore things related to applied intelligence and the internet of things. AINTLab is a hub for learning and innovation in applied intelligence and IoT. Pioneering Artificial Intelligence research, AINTLab focuses on machine learning, deep learning, IoT, and self-supervised learning. Our expertise drives agricultural innovation, vessel detection, human action recognition, and predictive analytics, promoting sustainable agriculture and global food security. Explore our extensive collection of research publications on AI, machine learning, IoT, and sustainable agriculture, featuring groundbreaking work on transformer models, predictive analytics, and more. Applied INTelligence Lab (AINTLab), Department of Artificial Intelligence and Data Science, Sejong University, Seoul 05006, Republic of Korea, Lead by Muhammad Syafrudin.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
