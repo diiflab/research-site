01636187c41d76d132b82a35f66e40a359cae504
Muhammad Syafrudin <2568729+justudin@users.noreply.github.com>
2026-09-02 15:06:37 +0900

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const {themes: prismThemes} = require('prism-react-renderer');
 const config = {
   title: 'Applied INtelligence (AIN) Lab',
   tagline: 'Applied INtelligence (AIN) Lab is not only a laboratory but also a playground to learn and explore things related to applied intelligence.',
-  url: 'https://ain.my.id',
+  url: 'https://ain.kookmin.ac.kr',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: '/img/favicon.png',
@@ -151,7 +151,7 @@ const config = {
         '@context': 'https://schema.org/',
         '@type': 'Organization',
         name: 'Applied INtelligence (AIN) Lab',
-        url: 'https://ain.my.id/',
+        url: 'https://ain.kookmin.ac.kr/',
         description: 'Applied INtelligence (AIN) Lab is not only a laboratory but also a playground to learn and explore things related to applied intelligence. We explore the frontier of artificial intelligence, data science, and intelligent systems. We design AI-driven solutions that connect systems, enhance communication, and promote sustainability. Through collaboration across academia and industry, we aim to build systems that shape a smarter, more connected world. AIN Lab is a hub for learning and innovation in applied intelligence and IoT. Pioneering Artificial Intelligence research, AIN Lab focuses on machine learning, deep learning, IoT, and self-supervised learning. Our expertise drives agricultural innovation, vessel detection, human action recognition, and predictive analytics, promoting sustainable agriculture and global food security. Explore our extensive collection of research publications on AI, machine learning, IoT, and sustainable agriculture, featuring groundbreaking work on transformer models, predictive analytics, and more.',
         foundingDate: '2019',
         founder: {
@@ -164,6 +164,8 @@ const config = {
             'https://scholar.google.co.kr/citations?user=WLTzkOMAAAAJ&hl=en',
             'https://ain.my.id/courses',
             'https://ain.my.id/',
+            'https://ain.kookmin.ac.kr/courses',
+            'https://ain.kookmin.ac.kr/',
           ]
         },
         address: {
